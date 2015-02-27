a1fccfa44e7be5c60aad0c4dffebe029ad25d63c
Michael <michael@bigroomstudios.com>
2015-02-27 14:32:19 -0500

    Change name/homepage.

--- src/enforce_scalars.functions.php
+++ src/enforce_scalars.functions.php
@@ -10,18 +10,21 @@
  * $values to be set if a scalar definition is set. Causes a Fatal Error by
  * default. Set $return to TRUE to simply validate the variables.
  *
- * @param Array $values The values to validate.
- * @param Array $scalars An associative array of Scalar types to validate in $values.
- * @param bool $allow_null Wetther or not to allow NULL values to pass through as valid (if scalar definition is set).
- * @param bool $return Wether to return (validate). Defaults to FALSE, or Fatal Error (same as Type Hinting).
+ * @param mixed[] $values The values to validate.
+ * @param mixed[] $scalars An associative array of Scalar types to validate in $values.
+ * @param bool $allow_null (optional) Wetther or not to allow NULL values to pass through as valid (if scalar definition is set).
+ * @param bool $return (optional) Wether to return (validate). Defaults to FALSE, or Fatal Error (same as Type Hinting).
  *
  * @return bool Wether all Scalar tests passed or not.
  *
  * @author Michael Mullligan <michael@bigroomstudios.com>
  */
-function enforce_scalars(Array $values, Array $scalars, $allow_null = NULL, $return = NULL) {
-	list($values, $scalars, $allow_null, $return) = fill_defaults(
-		func_get_args(), array(2 => TRUE, 3 => FALSE));
+function enforce_scalars(Array $values, Array $scalars, Array $options = NULL) {
+	$options = fill_defaults((array) $options, array(
+			'allow_null' => TRUE,
+			'return' => FALSE,
+			'soft_numeric' => FALSE
+	));
 	$error = FALSE;
 	foreach($scalars as $key => $scalar_type) {
 		$error_message = '';
@@ -32,32 +35,65 @@ function enforce_scalars(Array $values, Array $scalars, $allow_null = NULL, $ret
 			trigger_error("Scalar Enforce Call-Error: Unexpected '".get_type($scalar_type)."', expected String or NULL.", E_USER_ERROR);
 		} else {
 			$scalar_type = strtolower($scalar_type);
-			if((!array_key_exists($key, $values) || is_null($values[$key])) && (bool) $allow_null) {
+			if((!array_key_exists($key, $values) || is_null($values[$key])) && (bool) $options['allow_null']) {
 				continue;
 			}
 			$value = array_key_exists($key, $values) ? $values[$key] : NULL;
-			if($scalar_type == 'null') {
-				if(!is_null($value)) {    $scalar_error = TRUE; }
-			} else if($scalar_type == 'float' || $scalar_type == 'real') {
-				if(!is_float($value)) {   $scalar_error = TRUE; }
-			} else if($scalar_type == 'int') {
-				if(!is_int($value)) {     $scalar_error = TRUE; }
-			} else if($scalar_type == 'numeric') {
-				if(!is_numeric($value)) { $scalar_error = TRUE; }
-			} else if($scalar_type == 'string') {
-				if(!is_string($value)) {  $scalar_error = TRUE; }
-			} else if($scalar_type == 'bool') {
-				if(!is_bool($value)) {    $scalar_error = TRUE; }
-			} else if($scalar_type == 'object') {
-				if(!is_object($value)) {  $scalar_error = TRUE; }
-			} else {
-				trigger_error("Scalar Enforce Call-Error: Unrecognized Scalar Type '$scalar_type'.", E_USER_ERROR);
+			switch($scalar_type) {
+				case 'array':
+					$scalar_error = !is_array($value);
+					break;
+				case 'bool':
+				case 'boolean':
+					$scalar_error = !is_bool($value);
+					break;
+				case 'float':
+				case 'double':
+				case 'real':
+					if($options['soft_numeric'] && $value == (float) $value) {
+						break;
+					}
+					$scalar_error = !is_float($value);
+					break;
+				case 'int':
+				case 'integer':
+					if($options['soft_numeric'] && $value == (int) $value) {
+						break;
+					}
+					$scalar_error = !is_int($value);
+					break;
+				case 'unset':
+				case 'null' :
+					$scalar_error = !is_null($value);
+					break;
+				case 'object':
+					$scalar_error = !is_object($value);
+					break;
+				case 'string':
+					$scalar_error = !is_string($value);
+					break;
+				case 'scalar':
+					$scalar_error = !is_scalar($value);
+					break;
+				case 'numeric':
+					$scalar_error = !is_numeric($value);
+					break;
+				case 'callable':
+					$scalar_error = !is_callable($value);
+					break;
+				case 'resource':
+					$scalar_error = !is_resource($value);
+					break;
+				default:
+					trigger_error("Scalar Enforce Call-Error: Unrecognized Scalar Type '$scalar_type'.", E_USER_ERROR);
 			}
+			
 			if($scalar_error) {
-				trigger_error("Scalar Enforcement Error: Expected scalar type '$scalar_type' for index $key, '".gettype($value)."' given.", ($return ? E_USER_WARNING : E_USER_ERROR));
+				$callers = called_from();
+				trigger_error("Scalar Enforcement Error: Expected scalar type '$scalar_type' for index $key, '".gettype($value)."' given. ({$callers['file']}:{$callers['line']})", ($options['return'] ? E_USER_WARNING : E_USER_ERROR));
 			}
 		}
 		$error = $error || $scalar_error;
 	}
-	return !$error;
+	return (bool) !$error;
 }
